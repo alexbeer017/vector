0610af9c2f4ebd8d097a590a1d3fd6c0de9a4484
alexbeer017 <77503559+alexbeer017@users.noreply.github.com>
2021-02-08 20:15:02 -0900

    Update rice.cpp

--- rice.cpp
+++ rice.cpp
@@ -1,4 +1,11 @@
-  #include <iostream>
+/**Alex Beer
+ * CS.201
+ * Jon Genetti
+ * 2/8/2021
+ * Rice.cpp is pretty much just squaring the previous number and having a cap at 1000000000
+ */
+
+#include <iostream>
 
 using namespace std;
 
